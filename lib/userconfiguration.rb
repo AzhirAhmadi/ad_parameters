54ceaaeb89cc384b525c2e7f051e8b0ca1c7af05
Azhir <azhir@buzzjective.com>
2024-02-17 18:15:45 +0100

    add protobuf objects

--- lib/userconfiguration.rb
+++ lib/userconfiguration.rb
@@ -5,40 +5,37 @@
 #
 require 'protobuf'
 
-module FYBER
-  module Userconfiguration
-    ::Protobuf::Optionable.inject(self) { ::Google::Protobuf::FileOptions }
+module Userconfiguration
+  ::Protobuf::Optionable.inject(self) { ::Google::Protobuf::FileOptions }
 
-    ##
-    # Message Classes
-    #
-    class Creative < ::Protobuf::Message; end
-    class CreativeSeq < ::Protobuf::Message; end
-    class Placement < ::Protobuf::Message; end
-    class PlacementSeq < ::Protobuf::Message; end
+  ##
+  # Message Classes
+  #
+  class Creative < ::Protobuf::Message; end
+  class CreativeSeq < ::Protobuf::Message; end
+  class Placement < ::Protobuf::Message; end
+  class PlacementSeq < ::Protobuf::Message; end
 
 
-    ##
-    # Message Fields
-    #
-    class Creative
-      optional :string, :id, 1
-      optional :float, :price, 2
-    end
+  ##
+  # Message Fields
+  #
+  class Creative
+    optional :string, :id, 1
+    optional :float, :price, 2
+  end
 
-    class CreativeSeq
-      repeated ::FYBER::Userconfiguration::Creative, :creative, 1
-    end
+  class CreativeSeq
+    repeated ::Userconfiguration::Creative, :creative, 1
+  end
 
-    class Placement
-      optional :string, :id, 1
-      repeated ::FYBER::Userconfiguration::Creative, :creative, 2
-    end
+  class Placement
+    optional :string, :id, 1
+    repeated ::Userconfiguration::Creative, :creative, 2
+  end
 
-    class PlacementSeq
-      repeated ::FYBER::Userconfiguration::Placement, :placement, 1
-    end
-
+  class PlacementSeq
+    repeated ::Userconfiguration::Placement, :placement, 1
   end
 
 end
